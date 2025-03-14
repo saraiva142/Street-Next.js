1ee4dfb63646b6eb1d83a011b694af3a6a9200ab
saraiva142 <saraivajandrade@gmail.com>
2025-03-14 18:25:25 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,116 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="app">
+      <section className="hero">
+        <div className="img">
+          <img src="/portraits/port1.jpg"/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="nav">
+          <p>Tour</p>
+          <p>Updates</p>
+          <p>Contact</p>
+          <p>Merch</p>
+        </div>
+
+      </section>
+
+      <section className="projects">
+        <div className="img">
+          <img src="/portraits/port10.jpg"/>
+        </div>
+
+        <div className="projects-brief">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis 
+            tellus ut hendrerit finibus. Nullam interdum bibendum scelerisque. Sed quis turpis mi. 
+            Vivamus vitae pulvinar nisl, venenatis tempus nunc. Vivamus tincidunt eget egestas.
+          </p>
+        </div>
+
+        <div className="col projects-cover">
+          <div className="img">
+            <img src="/portraits/port4.jpg"/>
+          </div>
+        </div>
+        <div className="col projects-list">
+          <div className="project">
+            <h1>Sunrise</h1>
+            <p>Apple Music / Spotify / Youtube</p>
+          </div>
+          <div className="project">
+            <h1>Sunrise</h1>
+            <p>Apple Music / Spotify / Youtube</p>
+          </div>
+          <div className="project">
+            <h1>Sunrise</h1>
+            <p>Apple Music / Spotify / Youtube</p>
+          </div>
+          <div className="project">
+            <h1>Sunrise</h1>
+            <p>Apple Music / Spotify / Youtube</p>
+          </div>
+        </div>
+      </section>
+      <section className="about">
+        <div className="col intro">
+          <p>Introduction</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Vestibulum in ante porttitor, feugiat dolor in, elementum turpis. 
+            Donec ut sagittis dolor.
+          </p>
+        </div>
+        <div className="col portrait">
+          <div className="portrait-container">
+            <img src="/portraits/port7.jpg"/>
+          </div>
+        </div>
+      </section>
+      <section className="banner">
+        <div className="img">
+          <img src="/portraits/port9.jpg"/>
+        </div>
+
+        <div className="banner-copy">
+          <p>Be the</p>
+          <h1>Root Street</h1>
+          <p>Join the community, take your sk8, bike or surf-board,
+            and let's hit the road together.  
+          </p>
+          <button>Join the newsletter</button>
+        </div>
+      </section>
+      <section className="footer">
+        <div className="col">
+          <p>Root Street</p>
+          <p>Instagram / TikTok / Discord</p>
+
+          <div className="footer-links">
+            <p>Menu</p>
+            <h1>Tour</h1>
+            <h1>Updates</h1>
+            <h1>Merch</h1>
+            <h1>Contact</h1>
+          </div>
+
+          <p>&copy; Designed by João Saraiva</p>
+        </div>
+
+        <div className="col">
+          <p>
+            Join the newsletter <br /> 
+            <button>Subscribe</button>
+          </p>
+
+          <div className="shop">
+            <div className="img">
+              <img src="/portraits/port8.jpg"/>
+            </div>
+          </div>
+
+          <p>Apple Music / Spotify / Youtube</p>
+        </div>
+      </section>
     </div>
   );
 }
